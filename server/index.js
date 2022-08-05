fb096de00e0178d6097df4727b4ed260fc5f096a
parkhyeonji <iamphj3@naver.com>
2022-08-06 04:45:07 +0900

    package.json 변경전

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express') // 모듈 가져옴
 const app = express() // 함수를 이용해 앱 만들기
 const port = 3001 // 아무렇게나 해도 됨
+const bodyParser = require('body-parser');
+const { User } = require("./models/Users");
+
+//application/x-www-form-urlencoded 분석해서 가져오기
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 분석해서 가져오기
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://hyeonji:eunpha@boilerplate.qfusp1f.mongodb.net/?retryWrites=true&w=majority', {
@@ -10,5 +18,18 @@ mongoose.connect('mongodb+srv://hyeonji:eunpha@boilerplate.qfusp1f.mongodb.net/?
 
 
 app.get('/', (req, res) => { res.send('Hello World!안녕하세요' ) }) // root 디렉토리에 Hello World 출력
+
+app.post('/register', (req, res) => {
+  // 회원가입 시 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다
+
+  const user = new User(req.body) // bodyparser를 통해 받아옴
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)}) // 3001번 포트에서 실행
