e38696230ccc7f3c535f0fbbdd717c7183ec4459
parkhyeonji <iamphj3@naver.com>
2022-08-07 01:26:46 +0900

    fix: Auth 기능 오타 수정

--- index.js
+++ index.js
@@ -83,4 +83,17 @@ app.get('/api/users/auth', auth , (req, res) => {
   })
 })
 
+app.get('/api/users/logout', auth , (req, res) => {
+
+  User.findByIdAndUpdate({ _id: req.user._id },
+    { token: "" }
+    , (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
+
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)}) // 3001번 포트에서 실행
