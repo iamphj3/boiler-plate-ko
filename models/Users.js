f5837e81d6823c81a49a206d7ff534c23e6490e9
parkhyeonji <iamphj3@naver.com>
2022-08-07 01:14:03 +0900

    feat: Auth 기능 추가

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -58,8 +59,38 @@ userSchema.methods.comparePassword = function(plainPassword, cb) {
     // 1을 암호화해서 2와 비교해야 함. 2를 복호화 할 수는 없음!
 
     bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
-        if(err) return cb(err),
-            cb(null, isMatch)
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+
+    // jsonwebtoken을 이용해서 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err);
+        cb(null, user)
+    })
+}
+
+userSchema.methods.findByToken = function ( token, cb ) {
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용해 유저를 찾은 후에
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOns({"_id": decoded, "token": token}, function (err, user){
+            if(err) return cb(err);
+            cb(null, user)
+
+        })
     })
 }
 
