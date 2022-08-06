9e02d79f5dfff14a04412a4fc27639f81fc21e90
parkhyeonji <iamphj3@naver.com>
2022-08-06 23:25:17 +0900

    feat : bcrypt로 비밀번호 암호화, 로그인 기능 구현

--- index.js
+++ index.js
@@ -13,26 +13,53 @@ app.use(bodyParser.urlencoded({extended: true}));
 //application/json 분석해서 가져오기
 app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { userInfo } = require('os');
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTOpology: true // 에러 방지, useCreateIndex: true, useFindAndModify: false -> 지원x
 }).then(() => console.log('MOongoDB Connexted...'))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => { res.send('Hello ~~  sWorld!~~안녕하세요' ) }) // root 디렉토리에 Hello World 출력
+app.get('/', (req, res) => { res.send('Hello World! 안녕하세요' ) }) // root 디렉토리에 Hello World 출력
 
+// register Route
+// 회원가입 시 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다
 app.post('/register', (req, res) => {
-  // 회원가입 시 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다
 
   const user = new User(req.body) // bodyparser를 통해 받아옴
-
   user.save((err, doc) => {
     if(err) return res.json({ success: false, err})
     return res.status(200).json({
       success: true
     })
   })
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    
+    // 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password , (err, isMatch ) => {
+      if(!isMatch)
+      return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+      
+      // 비밀번호도 맞다면 토큰을 생성
+      user.generateToken((err, user) => {
+        
+      })
+
+    })
+
+  })
+
 })
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)}) // 3001번 포트에서 실행
--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,36 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ){
+    var user = this;
+    // 비밀번호를 바꿀때만 암호화
+    if (user.isModified('password')) {
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                // Store hash in your password DB.
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else { // 비밀번호가 아닌 다른 걸 바꿀 때
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // (1) plainPassword 1234567 , (2) 암호화된 비밀번호 $2b$10$E99al3CRPIHGfJA.yOqmF.ogRCU0kt/ppDwB20B6ehnpA9WoMzrQy
+    // 1을 암호화해서 2와 비교해야 함. 2를 복호화 할 수는 없음!
+
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err),
+            cb(null, isMatch)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
