70a20cb8c236fdd68e8d0370f8118f24a64b8d7d
parkhyeonji <iamphj3@naver.com>
2022-08-09 02:09:51 +0900

    fix: 서버연결, react dom 에러 고쳤다~

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
+
 import 'antd/dist/antd.css';
+
+import { Provider } from 'react-redux';
 import { applyMiddleware, createStore} from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from "redux-thunk";
@@ -13,8 +15,8 @@ import Reducer from './_reducers';
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   
   <Provider
     store={createStoreWithMiddleware(Reducer, 
@@ -24,7 +26,7 @@ ReactDOM.render(
   >
     <App />
   </Provider>
-  , document.getElementById('root'));
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
