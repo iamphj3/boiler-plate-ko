ab8fa617fed088b133e8f12729ccea3542b02a22
parkhyeonji <iamphj3@naver.com>
2022-08-10 20:26:21 +0900

    delete: node_modules 2 삭제

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Axios } from 'axios';
+import{ useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import { useNavigate } from 'react-router-dom';
 
-function LoginPage() {
+function LoginPage(props) 
+{
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  }
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  }
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    console.log('Email', Email)
+    console.log('Password', Password)
+
+    let body = {
+      email: Email,
+      password: Password
+    }
+
+    dispatch(loginUser(body))
+      .then(response => {
+        if(response.payload.loginSuccess) {
+          navigate('/');
+        } else {
+          alert('Error')
+        }
+      })
+    // // 서버에 보내기
+    // Axios.post('/api/users/login', body)
+    //   .then(response => {
+
+    //   })
+    
+  }
+  
   return (
-    <div>
-      LoginPage 로그인 페이지
+    <div style={{ 
+      display: 'flex', justifyContent: 'center', alignItems: 'center'
+      , width: '100%', height: '100vh'
+    }}>
+      
+      <form style={{display: 'flex', flexDirection: 'column'}}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+
+        <br />
+        <button>
+          Login
+        </button>
+      </form>
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore} from 'redux';
